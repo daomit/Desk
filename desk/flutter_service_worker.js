afabcd74e7a13433762d0a413d10d1524936c7fc
daomit <105905576+daomit@users.noreply.github.com>
2022-05-31 13:10:41 +0900

    desk update(2)

--- desk/flutter_service_worker.js
+++ desk/flutter_service_worker.js
@@ -88,8 +88,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "5c491c3df8d1f31f526c1703891e45a5",
-"/": "5c491c3df8d1f31f526c1703891e45a5",
+"index.html": "c6456cb38d7528ae4531063159753d97",
+"/": "c6456cb38d7528ae4531063159753d97",
 "main.dart.js": "ef6fded21c8e8a431aeb7708577f86b7",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
