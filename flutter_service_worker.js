b3f704ee32ce24fc5cfc9c3bbe913fb28fdf4d45
daomit <105905576+daomit@users.noreply.github.com>
2022-06-08 14:33:14 +0900

    home update(1)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -96,8 +96,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "b0c4df3647ddccbc9d5eb6674bd79a45",
-"/": "b0c4df3647ddccbc9d5eb6674bd79a45",
+"index.html": "8d7b72f05166eaa7b47b420f0174ff8d",
+"/": "8d7b72f05166eaa7b47b420f0174ff8d",
 "main.dart.js": "a79d0ec5dbdc08ab306e0ab778912edd",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
