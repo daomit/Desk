d72b62187a13424d3631fb4b03b138f48c3c4fcf
daomit <105905576+daomit@users.noreply.github.com>
2022-06-09 13:21:36 +0900

    home update 5

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -31,29 +31,14 @@ messaging.setBackgroundMessageHandler(function (payload) {
             }
         })
         .then(() => {
-            return registration.showNotification("새로운 메세지");
+            const title = payload.notification.title;
+            const options = {
+                body: payload.notification.score
+              };
+            return registration.showNotification(title, options);
         });
     return promiseChain;
 });
 self.addEventListener('notificationclick', function (event) {
-    console.log('On notification click: ', event.notification.tag);
-    event.notification.close();
-
-    // This looks to see if the current is already open and
-    // focuses if it is
-    event.waitUntil(clients.matchAll({
-      type: "window"
-    }).then(function(clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
-        if (client.url == '/' && 'focus' in client)
-          return client.focus();
-      }
-      if (clients.openWindow)
-        return clients.openWindow('/');
-    }));
-
-
-    // alert('notification received: ', event);
-    // console.log('notification received: ', event);
+    console.log('notification received: ', event)
 });
