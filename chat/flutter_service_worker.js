b945bb9a3e96fd90fa2dedf017b7b8b92c6bf6e4
daomit <105905576+daomit@users.noreply.github.com>
2022-06-09 11:40:30 +0900

    220609 chat update 3

--- chat/flutter_service_worker.js
+++ chat/flutter_service_worker.js
@@ -79,9 +79,9 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "a8cda88f4aca763ad700c0c15da80eeb",
-"/": "a8cda88f4aca763ad700c0c15da80eeb",
-"main.dart.js": "6f8120e3cf63e2667355fe3e7fe33991",
+"index.html": "9000059f4637c25bc5dda1e29c1bd373",
+"/": "9000059f4637c25bc5dda1e29c1bd373",
+"main.dart.js": "0bbaa7b812f8da6616f91e1551102f1f",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
 };
