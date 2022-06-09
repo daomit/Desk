70c1812dd25b0826d34cc370c74ae58ec6dcdbf8
daomit <105905576+daomit@users.noreply.github.com>
2022-06-09 11:28:16 +0900

    home chat update 2

--- chat/flutter_service_worker.js
+++ chat/flutter_service_worker.js
@@ -79,8 +79,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "7d8628dfa100ffb6b5881832274f4ce1",
-"/": "7d8628dfa100ffb6b5881832274f4ce1",
+"index.html": "a8cda88f4aca763ad700c0c15da80eeb",
+"/": "a8cda88f4aca763ad700c0c15da80eeb",
 "main.dart.js": "6f8120e3cf63e2667355fe3e7fe33991",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -96,8 +96,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "ad99938a5f5ae30cd86b3dfc0a7f2c24",
-"/": "ad99938a5f5ae30cd86b3dfc0a7f2c24",
+"index.html": "c83197addde30a337033c0a32862942a",
+"/": "c83197addde30a337033c0a32862942a",
 "main.dart.js": "e0bb1dd415ecd0c59cb53fb06506c108",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
