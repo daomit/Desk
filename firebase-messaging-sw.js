e60182f874848ca4c65c34d4c952042317370592
daomit <105905576+daomit@users.noreply.github.com>
2022-06-09 15:05:43 +0900

    chat home update 6

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,32 +13,6 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((message) => {
-  console.log("onBackgroundMessage", message);
-});
-
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const promiseChain = clients
-        .matchAll({
-            type: "window",
-            includeUncontrolled: true
-        })
-        .then(windowClients => {
-            for (let i = 0; i < windowClients.length; i++) {
-                const windowClient = windowClients[i];
-                windowClient.postMessage(payload);
-            }
-        })
-        .then(() => {
-            const title = payload.notification.title;
-            const options = {
-                body: payload.notification.score
-              };
-            return registration.showNotification(title, options);
-        });
-    return promiseChain;
-});
 self.addEventListener('notificationclick', function (event) {
     console.log('notification received: ', event)
 });
