dc69c9a94f014048e432d4c9d2adc894430383ab
daomit <daomitinfo@gmail.com>
2022-08-05 10:48:22 +0900

    2022.08.05-10:48

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -96,8 +96,8 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "img.gif": "50c5e3e79b276c92df6cc52caeb464f0",
-"index.html": "fe239cb248935ffc8fd25709b5b382f8",
-"/": "fe239cb248935ffc8fd25709b5b382f8",
+"index.html": "128637756d45524819f7951d019483ef",
+"/": "128637756d45524819f7951d019483ef",
 "main.dart.js": "74313734515645e6b2bb6ead79081af2",
 "manifest.json": "2b5ba2778470d390063c60bd60a04198",
 "version.json": "08b8eb61b274f462c6b2978d93c998e1"
