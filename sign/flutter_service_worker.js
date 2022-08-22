d041751fe3e723294744f617ea99667b015344d8
daomit <daomitinfo@gmail.com>
2022-08-22 16:04:30 +0900

    2022.08.22 - 16:04

--- sign/flutter_service_worker.js
+++ sign/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {
 "assets/fonts/SUIT-Light.ttf": "f4ec7396c79a340bf73e1a78f6df3eaf",
 "assets/fonts/SUIT-Regular.ttf": "ecaa6c57f4e7ed41445e86c876867096",
 "assets/fonts/SUIT-SemiBold.ttf": "e90281959674f17c627f0016e7b9e70f",
-"assets/NOTICES": "99b2f84d0078721d09d9f6c4dd5acc06",
+"assets/NOTICES": "ceadf352047af3eb730c72066fd42494",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -34,9 +34,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ca34c3eddbcaf8292016ddf1de5a164c",
-"/": "ca34c3eddbcaf8292016ddf1de5a164c",
-"main.dart.js": "c1cef2f5096849c1f33efc7fc9ec2ee4",
+"index.html": "2a65263eb4e66a3120c0692b3b470758",
+"/": "2a65263eb4e66a3120c0692b3b470758",
+"main.dart.js": "4895e7cd806312f911593f2716733d47",
 "manifest.json": "9c9d983b13fa6b232882a73ab76c2531",
 "version.json": "860b6e80237f5dfacb87264bad953c32"
 };
